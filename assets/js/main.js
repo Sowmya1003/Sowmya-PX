bb82bfa7bccbdc3ba7185ae768255d933f4b7e89
G Sowmya Sri <sowmyasrigundameedi@gmail.com>
2021-11-10 19:25:39 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ const accounts = [
 	(n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-DT38BLOGLNV8-2");
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-HRCQY4OHZUAN-2");
 
 
 
